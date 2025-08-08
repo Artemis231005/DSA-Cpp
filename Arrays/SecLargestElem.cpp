0e7654c46459ceb6795d3c20edfc797b39002b81
Artemis231005 <ishita.saraswat19@gmail.com>
2025-08-08 15:10:28 +0530

    Create Second Largest Element.cpp

---
+++ Arrays/SecLargestElem.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, arr[100];
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int max = arr[0];
+    int max2 = arr[0];
+    for (int i=0; i<n; i++) {
+        if (arr[i] > max) {
+            max2 = max;
+            max = arr[i];
+        }
+    }
+
+    cout << "Max element in array is: " << max;
+    return 0;
+}
