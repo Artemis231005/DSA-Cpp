44dd14f087c6da9eb584713087fdc99033e621e5
Artemis231005 <ishita.saraswat19@gmail.com>
2025-09-14 20:15:41 +0530

    Add levelOrderStored in LevelOrderBFS.cpp

--- Trees/Binary Trees/LevelOrderBFS.cpp
+++ Trees/Binary Trees/LevelOrderBFS.cpp
@@ -79,8 +79,38 @@ public:
             cout << endl;
         }
     }
+
     // Can also store result in vector<vector<int>> and push_back value in iteration(for loop) to vector<int> level (individual values 
     // in level) and push_back level in ans for each iteration of while loop (each level of tree)
+
+    vector<vector<int>> levelOrderStored(Node* root) {   // TC: O(N), SC: O(N)
+        vector<vector<int>> result;
+        if (root == NULL) {
+            return result;
+        }
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;   
+
+            for (int i = 0; i < size; i++) {
+                Node* temp = q.front();
+                q.pop();
+
+                level.push_back(temp->data);
+                if (temp->left != NULL) {
+                    q.push(temp->left);
+                }
+                if (temp->right != NULL) {
+                    q.push(temp->right);
+                }
+            }
+            result.push_back(level);
+        }
+        return result;
+    }
 };
 
 int main() {
@@ -96,7 +126,16 @@ int main() {
     tree.insertNode(9);
     tree.insertNode(10);
     
-    cout << "Level order traversal: " << endl;
+    cout << "Level order traversal directly: " << endl;
     tree.levelOrder(tree.root);
+
+    cout << "Level order traversal after storing: " << endl;
+    vector<vector<int>> ans = tree.levelOrderStored(tree.root);
+    for (auto &level : ans) {               // copying is expensive for big obj such as vector<int>, so use reference
+        for (int val : level) {             // doesn't really matter for small types such as int
+            cout << val << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
