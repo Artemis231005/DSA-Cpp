9601a5b6d0b6e996436c421c4e9ff96b2bad6d8e
Artemis231005 <ishita.saraswat19@gmail.com>
2025-09-23 22:26:22 +0530

    Create MaxPathSum.cpp

--- Trees/Binary Trees/MaxPathSum.cpp
+++ Trees/Binary Trees/MaxPathSum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <climits>
 #include <vector>
 #include <string>
 using namespace std;
@@ -51,6 +52,25 @@ public:
             i++;
         }
     }
+
+    int maxPathSumCalculate (Node* root) {
+        int maxSum = INT_MIN;           // INT_MIN so that we can handle negative values and possible negative path sum
+        maxPathSumHelper(root, maxSum);
+        return maxSum;
+    }
+
+    int maxPathSumHelper (Node* node, int &maxSum) {
+        if (node == NULL) {
+            return 0;
+        }
+
+        int lsum = max(0, maxPathSumHelper(node->left, maxSum)); // 0 used so that we can ignore -ve value if it comes from leaf node
+        int rsum = max(0, maxPathSumHelper(node->right, maxSum));
+        // If both sides are -ve, still better to take as 0 and ence return node->data since that will still be longest path (just that single node)
+        maxSum = max (maxSum, node->data + lsum + rsum);
+
+        return max(lsum, rsum) + node->data;
+    }
 };
 
 int main() {
@@ -64,7 +84,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+
     tree.insertNode(arr);
 
+    int maxPathSum = tree.maxPathSumCalculate(tree.root);
+    cout << "Maximum path sum for this tree is: " << maxPathSum << endl;
     return 0;
 }
