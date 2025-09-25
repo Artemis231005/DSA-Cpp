2375044329e806e9935418a0c3f690360b083fb7
Artemis231005 <ishita.saraswat19@gmail.com>
2025-09-25 20:40:11 +0530

    Create InsertNode.cpp

---
+++ Trees/Binary Trees/Binary Search Tree/InsertNode.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class BST {
+public:
+    Node* root;
+
+    BST() {
+        root = NULL;
+    }
+
+    void insertNode(int val) {
+        Node* newNode = new Node(val);
+        if (root == NULL) {
+            root = newNode;
+            return;
+        }
+
+        Node* temp = root;
+        while (temp != NULL) {
+            if (temp->data < val) {
+                if (temp->right != NULL) {
+                    temp = temp->right;
+                } else {
+                    temp->right = newNode;
+                    return;
+                }
+            } else {
+                if (temp->left != NULL) {
+                    temp = temp->left;
+                } else {
+                    temp->left = newNode;
+                    return;
+                }
+            }
+        }
+    }
+
+    void inorderTraversal(Node* node) {
+        if (node == NULL) {
+            return;
+        }
+        inorderTraversal(node->left);
+        cout << node->data << " ";
+        inorderTraversal(node->right);
+    }
+};
+
+int main() {
+    BST tree;
+    tree.insertNode(6);
+    tree.insertNode(3);
+    tree.insertNode(8);
+    tree.insertNode(2);
+    tree.insertNode(4);
+    tree.insertNode(7);
+
+    tree.inorderTraversal(tree.root);
+    return 0;
+}
