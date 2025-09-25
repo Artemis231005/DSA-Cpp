b716bf3cf47a7e5f07424063e4acc80add3752bd
Artemis231005 <ishita.saraswat19@gmail.com>
2025-09-25 21:23:56 +0530

    Create ceil.cpp

--- Trees/Binary Trees/Binary Search Tree/Ceil.cpp
+++ Trees/Binary Trees/Binary Search Tree/Ceil.cpp
@@ -47,6 +47,26 @@ public:
             }
         }
     }
+
+    // Ceil is lowest possible value greater tahn key such that ceilVal >= key AND ceilVal -> minimun
+    int findCeil (Node* root, int key) {
+        int ceil = -1;
+
+        while(root) {
+            if (root->data == key) {
+                ceil = root->data;
+                return root->data;
+            }
+
+            if (key > root->data) {
+                root = root->right;
+            } else {
+                ceil = root->data;
+                root = root->left;
+            }
+        }
+        return ceil;
+    }
 };
 
 int main() {
@@ -58,5 +78,13 @@ int main() {
     tree.insertNode(4);
     tree.insertNode(7);
 
+    int key;
+    cout << "Enter value for which you want to find ceil: ";
+    cin >> key;
+    if (tree.findCeil(tree.root, key) == -1) {
+        cout << "Ceil for " << key << " not foundin the tree" << endl;
+    } else {
+        cout << "Ceil for " << key << " will be: " << tree.findCeil(tree.root, key) << endl;
+    }
     return 0;
 }
