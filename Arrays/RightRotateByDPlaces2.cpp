91464e1c36b971a96d1037372336a18da779cbef
Artemis231005 <ishita.saraswat19@gmail.com>
2025-11-15 20:44:20 +0530

    Create RightRotateByDPlaces2.cpp

---
+++ Arrays/RightRotateByDPlaces2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void rightRotateByDPlaces(vector<int> &arr, int n, int d) {          // TC: O(N) Extra SC: O(1)
+    d = d % n;  
+    reverse(arr.begin(), arr.begin() + n);                          
+    reverse(arr.begin(), arr.begin() + d);    
+    reverse(arr.begin() + d, arr.begin() + n);
+}
+
+void display(vector<int> vec) {
+    for (int i: vec) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    int d;
+    cout << "Enter size: ";
+    cin >> n;
+
+    vector<int> vec(n);                     
+
+    cout << "Enter the values: ";
+    for (int i=0; i<n; i++) {
+        cin >> vec[i];
+    }
+
+    cout << "Original vector: ";
+    display(vec);
+
+    cout << "Enter no of places to rotate vector by: ";
+    cin >> d;
+    rightRotateByDPlaces(vec, n, d);
+    cout << "After right rotation by " << d << ", vector: ";
+    display(vec);
+    return 0;
+}
