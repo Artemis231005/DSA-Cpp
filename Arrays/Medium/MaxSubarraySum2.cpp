02f87459292a2721cccfab922f354e4595e89145
Artemis231005 <ishita.saraswat19@gmail.com>
2026-01-04 19:37:10 +0530

    Create MaxSubarraySum2.cpp

---
+++ Arrays/Medium/MaxSubarraySum2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int maxSubarraySum (vector<int> vec) {      // 
+    int maxSum = INT_MIN;
+
+    for (int i=0; i<vec.size(); i++) {
+        int sum = 0;
+        for (int j=i; j<vec.size(); j++) {
+            sum += vec[j];
+            maxSum = max(sum, maxSum);
+        }
+    }
+    return maxSum;
+}
+
+void display(vector<int> &vec) {
+    for (int i: vec) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of the array: ";
+    cin >> n;
+
+    vector<int> vec(n);                     
+    cout << "Enter the values: ";
+    for (int i=0; i<n; i++) {
+        cin >> vec[i];
+    }
+
+    cout << "The maximum subarray sum is: " << maxSubarraySum(vec);
+    return 0;
+}
