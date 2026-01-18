2fe5fa28720f6229d02a32a972929a72b1721908
Artemis231005 <ishita.saraswat19@gmail.com>
2026-01-18 21:02:32 +0530

    Create SetMatrixZero4.cpp

---
+++ Arrays/Medium/SetMatrixZero3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void setMatrixZeros(vector<vector<int>> &vec) {     // TC: O(2*n*m)   SC: O(1)
+    int n = vec.size();
+    int m = vec[0].size();
+    // col = matrix[0][...]   (first row)
+    // row = matrix[...][0]   (first col)
+    int col0 = 1;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (vec[i][j] == 0) {
+                // mark i-th row
+                vec[i][0] = 0;
+
+                // mark j-th col
+                if (j != 0) {
+                    vec[0][j] = 0;
+                } else {
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+    for (int i=1; i<n; i++) {
+        for (int j=1; j<n; j++) {
+            if(vec[i][j] != 0) {
+                if(vec[0][j] == 0 || vec[i][0] == 0) {
+                    vec[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if(vec[0][0] == 0) {
+        for (int j=0; j<n; j++) {
+            vec[0][j] = 0;
+        }
+        if (col0 == 0) {
+            for (int i=0; i<n; i++) {
+                vec[i][0] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    cout << "Enter number of columns: ";
+    cin >> m;
+
+    vector<vector<int>> vec(n, vector<int>(m));
+    cout << "Enter values: ";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> vec[i][j];
+        }
+    }
+
+    setMatrixZeros(vec);
+    cout << "Resultant matrix of matrix with rows and columns set to 0 for 0 values: " << endl;
+    for (auto &row : vec) {
+        for (int x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
