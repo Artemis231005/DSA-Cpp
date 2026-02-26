a923047150dba937bcaa5e80eab8d638c7d6b717
Artemis231005 <ishita.saraswat19@gmail.com>
2026-02-26 17:10:44 +0530

    Sorting in Folders

--- test.cpp
+++ test.cpp
@@ -1,75 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <climits>
 using namespace std;
 
-bool bfs(int s, int t, vector<vector<int>> &residual,
-         vector<int> &parent, int V) {
-
-    vector<bool> visited(V, false);
-    queue<int> q;
-
-    q.push(s);
-    visited[s] = true;
-    parent[s] = -1;
-
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-
-        for (int v = 0; v < V; v++) {
-            if (!visited[v] && residual[u][v] > 0) {
-                parent[v] = u;
-                visited[v] = true;
-                if (v == t)
-                    return true;
-                q.push(v);
-            }
-        }
+void findPermutations(int idx, vector<int>& vec, vector<vector<int>>& ans) {
+    if (idx == vec.size()) {
+        ans.push_back(vec);
+        return;
     }
-    return false;
+
+    for(int i=idx; i<vec.size(); i++) {
+        swap(vec[idx], vec[i]);
+        findPermutations(idx+1, vec, ans);
+        swap(vec[idx], vec[i]);
+    }
 }
 
-/* Maximum number of edge-disjoint paths = Max Flow
-   (all edges have capacity 1) */
-int maxEdgeDisjointPaths(vector<vector<int>> &graph, int s, int t, int V) {
-    vector<vector<int>> residual = graph;
-    vector<int> parent(V);
-    int maxFlow = 0;
-
-    while (bfs(s, t, residual, parent, V)) {
-        int pathFlow = INT_MAX;
-
-        for (int v = t; v != s; v = parent[v]) {
-            int u = parent[v];
-            pathFlow = min(pathFlow, residual[u][v]);
-        }
-
-        for (int v = t; v != s; v = parent[v]) {
-            int u = parent[v];
-            residual[u][v] -= pathFlow;
-            residual[v][u] += pathFlow;
-        }
-
-        maxFlow += pathFlow;
-    }
-    return maxFlow;
+vector<vector<int>> permute(vector<int> vec) {
+    vector<vector<int>> ans;
+    findPermutations(0, vec, ans);
+    return ans;
 }
 
 int main() {
-    int V = 6;          
-    int s = 0, t = 5;   
+    int n, val;
+    vector<int> vec;
 
-    vector<vector<int>> graph = {
-        {0,1,1,0,0,0},
-        {0,0,1,1,0,0},
-        {0,0,0,1,1,0},
-        {0,0,0,0,1,1},
-        {0,0,0,0,0,1},
-        {0,0,0,0,0,0}
-    };
+    cout << "Enter size of vector: ";
+    cin >> n;
 
-    cout << maxEdgeDisjointPaths(graph, s, t, V);
+    cout <<"Enter values: ";
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        vec.push_back(val);
+    }
+
+    cout << "The permutations are: " << endl;
+    vector<vector<int>> ans = permute(vec);
+    for (vector<int> i : ans) {
+        for (int j : i) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
